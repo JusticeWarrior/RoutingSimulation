181e4be3c45057a91647ac4b84b9390a563e113a
Jordan P Huffaker <jhuffak@ecegrid-thin4.ecn.purdue.edu>
2016-10-27 17:24:30 -0400

    Fixed some bugs in the initialization function

--- routingtable.c
+++ routingtable.c
@@ -5,18 +5,18 @@ struct route_entry routingTable[MAX_ROUTERS];
 int NumRoutes;
 
 void InitRoutingTbl (struct pkt_INIT_RESPONSE *InitResponse, int myID) {
-	ntoh_pkt_INIT_RESPONSE(InitResponse);
-
-	routingTable[i].dest_id = myID;
-	routingTable[i].next_hop = myID;
-	routingTable[i].cost = 0;
+	routingTable[0].dest_id = myID;
+	routingTable[0].next_hop = myID;
+	routingTable[0].cost = 0;
 
 	int i;
 	for (i = 1; i <= InitResponse->no_nbr; i++) {
 		routingTable[i].dest_id = InitResponse->nbrcost[i - 1].nbr;
 		routingTable[i].next_hop = InitResponse->nbrcost[i - 1].nbr;
-		routingtable[i].cost = InitResponse->nbrcost[i - 1].cost;
+		routingTable[i].cost = InitResponse->nbrcost[i - 1].cost;
 	}
+
+	NumRoutes = InitResponse->no_nbr + 1;
 }
 
 int UpdateRoutes(struct pkt_RT_UPDATE *RecvdUpdatePacket, int costToNbr, int myID) { return 0; }
