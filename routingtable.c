2fdb15c56187de3869242036eb11552e61b9fd67
Jordan P Huffaker <jhuffak@ecegrid-thin4.ecn.purdue.edu>
2016-10-27 16:59:32 -0400

    Done with the first function

--- routingtable.c
+++ routingtable.c
@@ -4,7 +4,20 @@
 struct route_entry routingTable[MAX_ROUTERS];
 int NumRoutes;
 
-void InitRoutingTbl (struct pkt_INIT_RESPONSE *InitResponse, int myID) { }
+void InitRoutingTbl (struct pkt_INIT_RESPONSE *InitResponse, int myID) {
+	ntoh_pkt_INIT_RESPONSE(InitResponse);
+
+	routingTable[i].dest_id = myID;
+	routingTable[i].next_hop = myID;
+	routingTable[i].cost = 0;
+
+	int i;
+	for (i = 1; i <= InitResponse->no_nbr; i++) {
+		routingTable[i].dest_id = InitResponse->nbrcost[i - 1].nbr;
+		routingTable[i].next_hop = InitResponse->nbrcost[i - 1].nbr;
+		routingtable[i].cost = InitResponse->nbrcost[i - 1].cost;
+	}
+}
 
 int UpdateRoutes(struct pkt_RT_UPDATE *RecvdUpdatePacket, int costToNbr, int myID) { return 0; }
 
